4252d9520790f798abc5fde278e8a296a07f8a13
unknown <Mallerly Rojas Cardenas>
2022-07-30 07:44:30 -0500

    se realiza una lista con numeros aleatorios entre 1 y 9

---
+++ src/ArrayBidimencional.java
@@ -0,0 +1,23 @@
+public class ArrayBidimencional {
+    public static void main(String[] args) {
+        int [][] numeros = new int [4][5];
+
+        for (int i = 0; i < numeros.length; i++){
+            for (int j =0; j< numeros[i].length; j++){
+                numeros[i][j] = (int) Math.floor(Math.random()* (9 -1));
+
+            }
+        }
+        for (int i = 0; i < numeros.length; i++) {
+            for (int j = 0; j < numeros[i].length; j++) {
+                System.out.print(numeros[i][j]+ "");
+
+            }
+            System.out.println("");
+
+        }
+
+
+    }
+
+}
