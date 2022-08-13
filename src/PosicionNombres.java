135ab880f5055201df9c363e2ed5dc5ddf22b432
unknown <Mallerly Rojas Cardenas>
2022-08-13 07:17:57 -0500

    se crea lista de contactos

--- src/PosicionNombres.java
+++ src/PosicionNombres.java
@@ -1,27 +1,28 @@
+import javax.swing.*;
+
 public class PosicionNombres {
     public static void main(String[] args) {
-        String[][] nombres = new String[3][4];
+        String[][] nombres = {
+                {"laura", "diego", "paola", "frank"},
+                {"leilany", "santiago", "jose", "kate"},
+                {"issabela", "caren", "ronal", "mallerly"}};
 
-        nombres[0][0] = "Laura";
-        nombres[1][0] = "Leilany";
-        nombres[2][0] = "Isabela";
-        nombres[0][1] = "Diego";
-        nombres[1][1] = "Santiago";
-        nombres[2][1] = "Caren";
-        nombres[0][2] = "Paola";
-        nombres[1][2] = "Jose";
-        nombres[2][2] = "Ronal";
-        nombres[0][3] = "Frank";
-        nombres[1][3] = "kate";
-        nombres[2][3] = "Mallerly";
+        int fila = (int) Math.floor(Math.random() * (2 -0 ));
+        int columna = (int) Math.floor(Math.random() * (3-0 ));
+
+        JOptionPane.showMessageDialog(null, nombres[fila][columna]);
 
 
-
-
-
-
-
-
+        }
 
     }
-}
+
+
+
+
+
+
+
+
+
+
